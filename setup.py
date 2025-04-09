32ab8c022820c4b70a17abfdc65ecd29ad443f98
Ethan Cleminson <128926337+Etmannn@users.noreply.github.com>
2025-04-09 12:19:26 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='EtmannUtils',
+  version='0.1',
+  url='httpsL//github.com/Etmannn/EtmannUtils',
+  author='Ethan Cleminson',
+  author_email='ethan.cleminso@gmail.com',
+  description="collection of small utility programs that I've made",
+  packages=find_packages()
+)
